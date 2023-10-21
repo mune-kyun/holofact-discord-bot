30833b8851c0691a5f1c0b6359c40b1aa8aad0a0
mune-kyun <rzknurfaizi@gmail.com>
2023-10-21 19:23:24 +0700

    vtuber [name] return real data

--- src/handler/vtuber/chatInputCommand.ts
+++ src/handler/vtuber/chatInputCommand.ts
@@ -3,7 +3,13 @@ import { getVtuberByName } from "../../api";
 const handleVtuber = (interaction, name) => {
   getVtuberByName(name)
     .then(({ vtuber }: any) => {
-      interaction.reply(`**${vtuber.name}** is kawaii \ndesu`);
+      const { name, birthday, greeting, height } = vtuber;
+      interaction.reply(`
+        Name: **${name}** 
+        \nBirthday: **${birthday}** 
+        \nGreeting: **${greeting}** 
+        \nHeight: **${height} cm** 
+        `);
     })
     .catch((e) => {
       interaction.reply(`error occured`);
