a10cc49a724d7a052c018fc40a893a3cf5d16af3
mune-kyun <rzknurfaizi@gmail.com>
2023-10-22 14:58:44 +0700

    vtuber result

--- src/handler/main/index.ts
+++ src/handler/main/index.ts
@@ -4,15 +4,16 @@ const mainHandleVtuberDetail = (interaction, name) => {
   getVtuberByName(name)
     .then(({ vtuber }: any) => {
       const { name, imgUrl, birthday, greeting, height } = vtuber;
-      interaction.reply(
-        `
+
+      interaction.reply({
+        content: `
         \nName: **${name}** 
         \nBirthday: **${birthday}** 
         \nGreeting: **${greeting}** 
         \nHeight: **${height} cm**
         \n${imgUrl}
-        `
-      );
+        `,
+      });
     })
     .catch((e) => {
       interaction.reply(`error occured`);
