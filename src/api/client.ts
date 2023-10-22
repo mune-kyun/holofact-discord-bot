d646ac2f717e996f24717ee0b6c46f7891bfa419
mune-kyun <rzknurfaizi@gmail.com>
2023-10-22 16:23:18 +0700

    env

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,7 @@
+require("dotenv").config();
 import { GraphQLClient } from "graphql-request";
 
-const endpoint = "http://localhost:4000/graphql/";
+const endpoint = process.env.GRAPHQL_SERVER;
 const client = new GraphQLClient(endpoint);
 
 export default client;
